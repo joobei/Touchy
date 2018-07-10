cbf63027228486f7f8608e5564751baac90cff67
Lukas Kern <lukas.kern2@gmail.com>
2018-07-10 12:02:34 +0200

    needed callback to update stuff

--- touchy.cpp
+++ touchy.cpp
@@ -17,167 +17,49 @@
 
 #define dllexport __declspec(dllexport)
 
-//#include <cstdio>
-//#include <cassert>
-//#include <conio.h>
-#include "vector_math.h"
-using namespace vmath;
-
-
-
 #include "HD/hd.h" //part of 3DS OpenHaptics
+#include <HDU/hduVector.h>
+#include <HDU/hduError.h>
 
 //Global variable and handle declarations
 HDSchedulerHandle hCallback;
 HHD hHD;
 
-//Sphere globals (only one sphere for now)
-double sphereRadius;
-vec3<double> spherePosition;
-int buttonState = -99;
-
-//Used to pause the sphere callback and thus force feedback
-
-/*******************************************************************************
-Generates a force to drive the cursor to the center of the sphere.
-*******************************************************************************/
-HDCallbackCode HDCALLBACK CallbackToSphereCenter(void *data)
+/* Holds data retrieved from HDAPI. */
+typedef struct
 {
-    // Stiffness, i.e. k value, of the sphere.  Higher stiffness results
-    // in a harder surface.
-    const double sphereStiffness = 1.f;
+	HDboolean m_buttonState;       /* Has the device button has been pressed. */
+	hduVector3Dd m_devicePosition; /* Current device coordinates. */
+	HDErrorInfo m_error;
+} DeviceData;
 
-    hdBeginFrame(hHD);
-
-    // Get the position of the device.
-    vec3<double> cursorPosition;
-    hdGetDoublev(HD_CURRENT_POSITION, cursorPosition);
-
-    // Find the distance between the device and the center of the
-    // sphere.
-    vec3<double> newvec = (cursorPosition - spherePosition);
-    double distance = length(newvec);
-
-    // If the user is within the sphere -- i.e. if the distance from the user to
-    // the center of the sphere is less than the sphere radius -- then the user
-    // is penetrating the sphere and a force should be commanded to repel him
-    // towards the surface.
-    if (distance < sphereRadius)
-    {
-        // Calculate the penetration distance.
-        double penetrationDistance = sphereRadius - distance;
-
-        // Create a unit vector in the direction of the force, this will always
-        // be outward from the center of the sphere through the user's
-        // position.
-        vec3<double> forceDirection = (spherePosition - cursorPosition) / distance;
-
-        // Use F=kx to create a force vector that is towards the center of
-        // the sphere and proportional to the penetration distance, and scaled
-        // by the object stiffness.
-        // Hooke's law explicitly:
-        double k = sphereStiffness;
-        vec3<double> x = penetrationDistance * forceDirection;
-        vec3<double> f = k * x;
-        hdSetDoublev(HD_CURRENT_FORCE, forceDirection);
-    }
-
-    hdEndFrame(hHD);
-
-    HDErrorInfo error;
-    if (HD_DEVICE_ERROR(error = hdGetError()))
-    {
-        if (error.errorCode == HD_SCHEDULER_FULL)
-        {
-            return HD_CALLBACK_DONE;
-        }
-    }
-
-    return HD_CALLBACK_CONTINUE;
-}
-
-/*******************************************************************************
-Generates an opposing force if the device attempts to penetrate the sphere.
-*******************************************************************************/
-HDCallbackCode HDCALLBACK FrictionlessSphereCallback(void *data)
-{
-    // Stiffness, i.e. k value, of the sphere.  Higher stiffness results
-    // in a harder surface.
-    const double sphereStiffness = 1.f;
-
-    hdBeginFrame(hHD);
-
-
-	// Get the position of the device.
-	vec3<double> cursorPosition;
-	hdGetDoublev(HD_CURRENT_POSITION, cursorPosition);
-
-	// Find the distance between the device and the center of the
-	// sphere.
-	vec3<double> newvec = (cursorPosition - spherePosition);
-	double distance = length(newvec);
-
-	// If the user is within the sphere -- i.e. if the distance from the user to
-	// the center of the sphere is less than the sphere radius -- then the user
-	// is penetrating the sphere and a force should be commanded to repel him
-	// towards the surface.
-	if (distance < sphereRadius)
-	{
-		// Calculate the penetration distance.
-		double penetrationDistance = sphereRadius - distance;
-
-		// Create a unit vector in the direction of the force, this will always
-		// be outward from the center of the sphere through the user's
-		// position.
-		vec3<double> forceDirection = (cursorPosition - spherePosition) / distance;
-
-		// Use F=kx to create a force vector that is away from the center of
-		// the sphere and proportional to the penetration distance, and scaled
-		// by the object stiffness.
-		// Hooke's law explicitly:
-		double k = sphereStiffness;
-		vec3<double> x = penetrationDistance * forceDirection;
-		vec3<double> f = k * x;
-		hdSetDoublev(HD_CURRENT_FORCE, f);
-	}
-
-    
-    hdEndFrame(hHD);
-
-
-    HDErrorInfo error;
-    if (HD_DEVICE_ERROR(error = hdGetError()))
-    {
-        if (error.errorCode == HD_SCHEDULER_FULL)
-        {
-            return HD_CALLBACK_DONE;
-        }
-    }
-
-    return HD_CALLBACK_CONTINUE;
-}
-
+static DeviceData gServoDeviceData;
 
 HDCallbackCode HDCALLBACK CallbackIdle(void *data)
 {
+	int nButtons = 0;
 	// Get the position of the device.
-	vec3<double> cursorPosition;
+	/*vec3<double> cursorPosition;*/
 	
 	hdBeginFrame(hHD);
 
-	hdGetDoublev(HD_CURRENT_POSITION, cursorPosition);
-	hdGetIntegerv(HD_CURRENT_BUTTONS, &buttonState);
+	/* Retrieve the current button(s). */
+	hdGetIntegerv(HD_CURRENT_BUTTONS, &nButtons);
+	
+	/* In order to get the specific button 1 state, we use a bitmask to
+	test for the HD_DEVICE_BUTTON_1 bit. */
+	gServoDeviceData.m_buttonState =
+		(nButtons & HD_DEVICE_BUTTON_1) ? HD_TRUE : HD_FALSE;
+
+	/* Get the current location of the device (HD_GET_CURRENT_POSITION)
+	We declare a vector of three doubles since hdGetDoublev returns
+	the information in a vector of size 3. */
+	hdGetDoublev(HD_CURRENT_POSITION, gServoDeviceData.m_devicePosition);
+
+	/* Also check the error state of HDAPI. */
+	gServoDeviceData.m_error = hdGetError();
 
 	hdEndFrame(hHD);
-
-	HDErrorInfo error;
-	if (HD_DEVICE_ERROR(error = hdGetError()))
-	{
-		if (error.errorCode == HD_SCHEDULER_FULL)
-		{
-			return HD_CALLBACK_DONE;
-		}
-	}
 
 	return HD_CALLBACK_CONTINUE;
 }
@@ -192,10 +74,6 @@ extern "C" dllexport int init() {
         return(error.errorCode);
     }
 
-    //initialize default sphere attributes so that variables are not empty
-    sphereRadius = 40.0;  //40 millimeters
-    spherePosition = vec3<double>(0, 0, 0);
-
     // Start the servo scheduler and enable forces.
     hdEnable(HD_FORCE_OUTPUT);
     hdStartScheduler();
@@ -203,8 +81,6 @@ extern "C" dllexport int init() {
     {
 		return(error.errorCode);
     }
-
-	
 
     return(HD_SUCCESS);
 }
@@ -220,20 +96,7 @@ extern "C" dllexport int startIdleCallback() {
 	return HD_SUCCESS;
 }
 
-extern "C" dllexport int startCenterCallback(double radius, double x, double y, double z) {
-    sphereRadius = radius;
-    spherePosition.x = x;
-    spherePosition.y = y;
-    spherePosition.z = z;
 
-    // Application loop - schedule our call to the main callback.
-    hCallback = hdScheduleAsynchronous(CallbackToSphereCenter, 0, HD_DEFAULT_SCHEDULER_PRIORITY);
-	
-	//Return success/error code
-	HDErrorInfo error;
-	error = hdGetError();
-	return(error.errorCode);
-}
 
 extern "C" dllexport int stopCallback() {
 	HDErrorInfo error;
@@ -257,57 +120,17 @@ extern "C" dllexport int stopCallback() {
 	return HD_SUCCESS;
 }
 
-extern "C" dllexport int startSphereCallback(double radius, double x, double y, double z) {
-    sphereRadius = radius;
-    spherePosition.x = x;
-    spherePosition.y = y;
-    spherePosition.z = z;
 
-    // Application loop - schedule our call to the main callback.
-    hCallback = hdScheduleAsynchronous(FrictionlessSphereCallback, 0, HD_DEFAULT_SCHEDULER_PRIORITY);
-	//Return success/error code
-	HDErrorInfo error;
-	error = hdGetError();
-	return(error.errorCode);
+extern "C" dllexport void getEEPosition(double positions[3])
+{
+	positions[0] = gServoDeviceData.m_devicePosition[0];
+	positions[1] = gServoDeviceData.m_devicePosition[1];
+	positions[2] = gServoDeviceData.m_devicePosition[2];
 }
 
-extern "C" dllexport void setSphereRadius(double radius)
+extern "C" dllexport bool getButtonState()
 {
-    sphereRadius = radius;
-}
-
-extern "C" dllexport double getSphereRadius()
-{
-    return sphereRadius;
-}
-
-
-extern "C" dllexport void setSpherePosition(double x, double y, double z)
-{
-    spherePosition.x = x;
-    spherePosition.y = y;
-    spherePosition.z = z;
-}
-
-extern "C" dllexport void getEEPosition(double position[3])
-{
-	//hdBeginFrame(hHD);
-    hdGetDoublev(HD_CURRENT_POSITION, position);
-	//hdEndFrame(hHD);
-}
-extern "C" dllexport int getButtonState(int bstate)
-{
-	//hdBeginFrame(hHD);
-    
-	/*hdGetIntegerv(HD_CURRENT_BUTTONS, &bstate);*/
-	bstate = buttonState;
-	return (HD_SUCCESS);
-	//hdEndFrame(hHD);
-
-	//Return success/error code
-	/*HDErrorInfo error;
-	error = hdGetError();
-	return(error.errorCode);*/
+	return gServoDeviceData.m_buttonState;
 }
 
 extern "C" dllexport int shutdown() {
